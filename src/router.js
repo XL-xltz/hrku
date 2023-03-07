07cad62c18d54e4e12b53266baf126142ce3d8dd
XL-xltz <804349447@qq.com>
2023-03-07 16:15:39 +0800

    基础 main 完成

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+// 登录
+const login = () => import(/* webpackChunkName: "about" */ "./views/login.vue");
 
 Vue.use(Router);
 
@@ -10,17 +11,17 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "login",
+      component: login,
     },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue"),
+    // },
+  ],
 });
---
+++ src/views/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  filters: {},
+  watch: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
